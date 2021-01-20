4f0813fb27e6f48932e6f5d291a6b3209fb92ea9
ljh468 <ljh468@naver.com>
2021-01-20 19:59:25 +0900

    210120

---
+++ to teach Java02/src/lecture01.java
@@ -0,0 +1,11 @@
+// 콘솔창에 내용 출력하기
+// 콘솔창에 Hello Java를 출력하시오.
+// ex. Hello Java
+public class lecture01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ to teach Java03/src/lecture02.java
@@ -0,0 +1,11 @@
+// 자신이 입력한 내용 출력하기
+// 자신의 이름을 입력하고 콘솔창결과에  Hello OOO!!를 출력하시오.
+// ex. Hello 이재훈!!
+public class lecture02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
