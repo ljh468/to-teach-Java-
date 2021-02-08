c8141bed487470a32efb7a7ece997c712d291ad2
ljh468 <ljh468@naver.com>
2021-02-08 10:38:09 +0900

    210208

--- to teach Java02/src/lecture00.java
+++ to teach Java02/src/lecture00.java
@@ -2,7 +2,9 @@
 public class lecture00 {
 
 	public static void main(String[] args) {
-		
+		for(int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
--- to teach Java02/src/lecture01.java
+++ to teach Java02/src/lecture01.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 // 1부터 n까지의 합을 출력하기
 public class lecture01 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int i = 1;
+		int n = sc.nextInt();
+		int sum = 0;
+		while(i <= n) {
+			sum += i;
+			i++;
+		}
+		System.out.println(sum);
 	}
 
 }
