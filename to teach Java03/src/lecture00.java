d59e00ccb4e0becbb477731e13ac7aab924c145f
ljh468 <ljh468@naver.com>
2021-02-13 23:39:30 +0900

    210213

--- to teach Java03/src/lecture00.java
+++ to teach Java03/src/lecture00.java
@@ -1,8 +1,25 @@
-// 배열
+// 배열  0, 10, 20, 30 ~ 100까지 10단위로 11개의 배열을 초기화하고
+// 배열에 저장된 값을 순서대로 출력하라
 public class lecture00 {
 
 	public static void main(String[] args) {
 		
+		/*
+		int[] arr = new int[11];
+		for(int i = 0; i <= 10; i++) {
+			arr[i] = i*10;
+		}
+		
+		for(int k=0; k < arr.length; k++) {
+		System.out.println(arr[k]);
+		}
+		*/
+		
+		int[] arr = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		
+		for(int i = 0; i <= 10; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
--- to teach Java03/src/lecture01.java
+++ to teach Java03/src/lecture01.java
@@ -1,8 +1,14 @@
-//
+// String 문자열 "데이터분석과"를 하나씩 쪼개서 문자열배열로 만들고 출력하라
+// 출력예시 : "데" "이" "터" "분" "석" "과"
 public class lecture01 {
 
 	public static void main(String[] args) {
+		String arr = "데이터분석과";
+		String[] str = arr.split("");
 		
+		for(int i = 0; i < str.length; i++) {
+			System.out.print("\"" +str[i] + "\" ");
+		}
 	}
 
 }
