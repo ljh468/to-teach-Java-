2ff169e25780c520d5fdc6e6801be2469d0c8722
ljh468 <ljh468@naver.com>
2021-02-16 17:53:06 +0900

    210216

--- to teach Java04/src/lecture00.java
+++ to teach Java04/src/lecture00.java
@@ -5,24 +5,7 @@ public class lecture00 {
 	public static void main(String[] args) {
 		
 		
-		int[] arr = new int[11];
-		
-		for(int i = 0; i <= 10; i++) {
-			arr[i] = i*10;
-		}
-		
-		for(int k=0; k < arr.length; k++) {
-		System.out.println(arr[k]);
-		}
-		
-		
-//		int[] arr = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-//		// 배열의 초기화
-//		
-//		for(int i = 0; i <= 10; i++) {
-//			System.out.println(arr[i]);
-//			//반복문을 이용해 배열 출력
-//		}
+	
 	}
 
 }
