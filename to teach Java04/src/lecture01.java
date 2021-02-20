f8a9ed1329efdd41943f29cc5008fdbc234830b6
ljh468 <ljh468@naver.com>
2021-02-20 23:35:57 +0900

    210220

--- to teach Java04/src/lecture01.java
+++ to teach Java04/src/lecture01.java
@@ -1,10 +1,28 @@
-// String 문자열 "데이터분석과"를 하나씩 쪼개서 문자열배열로 만들고 출력하라
-// 출력예시 : "데" "이" "터" "분" "석" "과"
+// List객체
+// Iterator
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class lecture01 {
-
 	public static void main(String[] args) {
+		List<String> rList = new ArrayList<String>();
+		rList.add("객체지향프로그래밍");
+		rList.add("하둡");
+		rList.add("MongoDB");
 		
-		
+		int i = 0;
+		Iterator<String> it = rList.iterator();
+		while(it.hasNext()) {
+			String str = it.next();
+//		    if(str.equals("객체지향프로그래밍")){
+			if(i==0) {
+				System.out.println("<"+str+">");
+			}else {
+				System.out.println(str);
+			}
+			i++;
+		}		
 	}
-
 }
+
--- to teach Java04/src/lecture02.java
+++ to teach Java04/src/lecture02.java
@@ -1,14 +1,12 @@
-// 배열의 총합과 평균
-// arr 배열에 54, 74, 80, 92, 100 의 정수를 넣고 
-// 배열의 총합과 평균을 구하라
-// 총합을 저장하기 위한 변수는 sum
-// 평균을 저장하기 위한 변수는 average 
+//  HashMap, ArrayList
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 public class lecture02 {
-
 	public static void main(String[] args) {
 		
-		
 	}
-
 }
