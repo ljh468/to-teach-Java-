74360061b9e0255aecf93811e072e9cb69846d9f
ljh468 <ljh468@naver.com>
2021-02-22 00:43:10 +0900

    210222

--- to teach Java04/src/lecture03.java
+++ to teach Java04/src/lecture03.java
@@ -1,7 +1,4 @@
-// 배열의 최대값과 최소값
-// arr  배열에 64, 79, 56, 32, 88, 99값을 초기화 하고 이배열에서 최소값과 최대값을 구하여 출력하라
-// 최소값은 min, 최대값은 max로 한다
-// 힌트는 반복문으로 돌아가면서 조건문을 사용하여 비교하면 됨
+
 
 public class lecture03 {
 	public static void main(String[] args) {
--- to teach Java04/src/lecture05.java
+++ to teach Java04/src/lecture05.java
@@ -1,6 +1,4 @@
-// 로또 번호 만들기
-// 보너스 문제 45개의 숫자를 임의로 지정해 6개의 숫자를 출력하라
-// Math.random()함수는 0.0 부터 1.0 사이의 랜덤한 난수를 발생시키는 함수
+
 public class lecture05 {
 
 	public static void main(String[] args) {
